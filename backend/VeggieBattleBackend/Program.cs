0aa451a602aa8e0e7236f703300afd370ce37de9
Maiska <42906162+Maiska123@users.noreply.github.com>
2022-10-16 15:29:54 +0300

    having problems with using of staticfiles - doesnt map json accordingly

--- backend/VeggieBattleBackend/Program.cs
+++ backend/VeggieBattleBackend/Program.cs
@@ -1,11 +1,18 @@
 using ApiHelper.Services;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.Extensions.FileProviders;
 
 var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder (args);
 
+builder.Services.AddAuthentication(
+    CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate();
+
 builder.Services.AddCors(options =>
 {
+    
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
@@ -29,16 +36,32 @@ builder.Services.AddSingleton<IImgProcessorService, ImgProcessorService> ();
 
 var app = builder.Build ();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment ()) {
     app.UseSwagger ();
     app.UseSwaggerUI ();
 }
 
+app.UseAuthentication();
 app.UseHttpsRedirection ();
 app.UseCors(MyAllowSpecificOrigins);
 // app.UseStaticFiles();
 // app.UseRouting();
+
+var cacheMaxAgeOneWeek = (60 * 60 * 24 * 7).ToString();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append(
+             "Cache-Control", $"public, max-age={cacheMaxAgeOneWeek}");
+    },    
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "staticFiles")),
+    RequestPath = "/StaticFiles"
+});
 
 app.UseAuthorization ();
 
